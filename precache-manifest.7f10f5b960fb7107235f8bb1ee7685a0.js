a1eb558ba385d4a8ecda5d567a432ddf9d9386b8
vo gia bao <vogiabao292@gmail.com>
2021-08-15 13:13:05 +0700

    Updates

---
+++ precache-manifest.7f10f5b960fb7107235f8bb1ee7685a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eab5f59d5f40b5e24f6973e92d34fc7",
+    "url": "/rmit-w7-project/index.html"
+  },
+  {
+    "revision": "b28204dbc023c5068d7e",
+    "url": "/rmit-w7-project/static/css/main.affd6a5a.chunk.css"
+  },
+  {
+    "revision": "735c0f2ecc57a7c33714",
+    "url": "/rmit-w7-project/static/js/2.fe0e3956.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rmit-w7-project/static/js/2.fe0e3956.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b28204dbc023c5068d7e",
+    "url": "/rmit-w7-project/static/js/main.58204211.chunk.js"
+  },
+  {
+    "revision": "d2918f811a37cd061fbd",
+    "url": "/rmit-w7-project/static/js/runtime-main.995f5c5c.js"
+  }
+]);
